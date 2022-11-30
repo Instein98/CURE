39794f251685001c18d01b58a8504f66ebe01d57
Instein98 <instein98@gmail.com>
2022-11-29 22:24:18 -0600

    print correct patches

--- src/tester/patchSampledMutants.py
+++ src/tester/patchSampledMutants.py
@@ -51,16 +51,24 @@ def getMutator(projPath: Path, mutId: str):
                 assert m is not None
                 return m[1]
 
+newSample={'collections': ['6152', '8112', '8343', '8216', '8348', '8108', '10087', '6148', '8492', '11864', '2652', '1508'], 'time': ['18136', '10081', '15493', '4864', '658', '12449', '2556', '19225', '10160', '2551', '4856', '8960', '11572'], 'math': ['94078', '118483', '94440', '108308', '118180', '94376', '115465', '41781', '94185', '117598', '115827', '94174', '92468', '109334', '118484', '94153', '117670', '108457', '107938', '118195', '115826', '35228', '16011', '118026', '27082', '1872', '35785', '39490', '116617', '95878', '35937', '111244', '110625', '39136', '74852', '110955', '64974', '63972', '22130', '7710', '116154', '101658', '110936', '117041', '84963', '35938', '109722', '38034', '111968', '17424', '15714', '34840', '41844', '60729', '95820', '57451', '117283', '81535', '19560', '117420', '117580', '111278', '103223', '34656', '119014', '19561', '35959', '5010', '108351', '27140', '113518', '48177', '108453', '32583', '111085', '111050', '103968', '96327', '77143', '58288', '116783', '14349', '42114', '15302', '14347', '80734', '36958', '8954', '16176', '3463', '62416', '118638', '42068', '94205', '117666', '94240', '94373', '93203', '66445', '118059', '14098', '108626', '18388', '118366', '118158', '62404', '94364', '59422', '93255', '118550', '108819', '2977'], 'codec': ['288', '691', '690', '669', '652', '1017', '497', '4217', '261', '297', '469', '316'], 'chart': ['70393', '70533', '34236', '76686', '34237', '54929'], 'jacksondatabind': ['927', '912', '7353', '40'], 'closure': ['48813', '31081', '31096', '13793', '30680', '13710', '36346', '90', '13942', '30743', '31012', '13724', '13866', '30845', '63', '36345', '144', '13957', '48224', '11209', '36349', '30923', '30740', '11445', '33315', '31023', '30953', '30739', '48791', '30709', '30952'], 'jacksonxml': ['292'], 'jxpath': ['12215', '9840', '7840', '10391', '10689', '10804', '8246', '9432', '10053', '7850', '10309', '7922'], 'jacksoncore': ['16309', '1370', '13101', '15722', '5817', '14170', '15781', '10690', '13052', '621', '15840', '10484', '1806', '15991', '15886', '15841', '10570', '11378', '345', '4570', '15799', '13888', '10988', '10606', '10516', '2147', '5447', '1367', '103', '12131', '290', '1366', '12861', '4441', '11980', '11868', '834', '6772', '11218', '3267', '16146', '10463', '12996', '15778', '7347', '3278', '14434', '11219', '6093', '14104', '14220', '12062'], 'compress': ['5465', '1623', '4844', '7964', '5574', '5847', '4899', '5371', '6028', '5858', '6121', '1504', '4853', '7940', '4816', '7966', '6174', '3497', '1607', '6128'], 'lang': ['7857', '7790', '7154', '6308', '7824', '21179', '15195', '15107', '14961', '15151', '17101', '6724', '6310', '15181', '5887', '5886', '7700', '7879', '3503', '6726', '16696']}
+
+generateForNewSample = False
+
 def main():
     for projPath in d4jProjPaths:
         projName = projPath.stem
 
-        # if ("math" not in projName):
-        #     continue
-
-        print('=' * 10 + " Start {} ".format(projName) + '=' * 10)
         mutIds=getSampledMutIdList(projPath)
+
+        if generateForNewSample:
+            simpleProjName = projName.split('-')[0]
+            if (simpleProjName not in newSample):
+                continue
+            mutIds = newSample[simpleProjName]
         
+        print('=' * 10 + " Start {} ".format(projName) + '=' * 10)
+
         mutDataDir = mutResultDirPath / (projName + '-mutants-allin')
         if (mutDataDir / 'reranked_patches.json').exists():
             print('reranked_patches.json exists in {}'.format(projName))
@@ -78,6 +86,7 @@ def main():
         recoverOutErr()
 
 def recoverAndCheckCorrectFixes():
+    correctPatches = []
     fixedDict = {}
     mutatorDict = {}
     for dir in mutResultDirPath.iterdir():
@@ -88,20 +97,18 @@ def recoverAndCheckCorrectFixes():
                 projName = dir.stem.split('-')[0]
                 projPathStem = re.match(r'(\w+?-\d+f).*', dir.stem)[1]
                 projPath = d4jProjDirPath / projPathStem
-                # print(str(projPath))
-                # print(projPath.exists())
-                recoverPatches(projPath, resultJson, dir / 'patches', projName, candidate_size=100, evenly=False)  # Todo: set candidate_size to 200 or 100?
+
+                # recoverPatches(projPath, resultJson, dir / 'patches', projName, candidate_size=100, evenly=False)  # Todo: set candidate_size to 200 or 100?
                 
                 patchDirPath = dir / 'patches'
-                fixedMidList = checkCorrectFix(projPath, patchDirPath)
+                fixedMidList, correctPatchList = checkCorrectFix(projPath, patchDirPath)
+                correctPatches.extend(correctPatchList)
                 fixedDict[projName] = fixedMidList
                 for mid in fixedMidList:
                     mutator = getMutator(projPath, mid)
                     if mutator not in mutatorDict:
                         mutatorDict[mutator] = []
                     mutatorDict[mutator].append(projName + '-' + mid)
-                # recoverPatches(projPath, resultJson, dir / 'patches-evenly', projName, candidate_size=200, evenly=True)
-                # patchDict = json.load()
     proj = [k for k in fixedDict]
     proj.sort()
     for key in proj:
@@ -109,10 +116,16 @@ def recoverAndCheckCorrectFixes():
     mutators = [k for k in mutatorDict]
     mutators.sort()
     for m in mutators:
+        print("{}: {}".format(m, mutatorDict[m]))
+    for m in mutators:
         print("{}: {}".format(m, len(mutatorDict[m])))
 
+    print('fixedDict = ' + str(fixedDict))
+    print('correctPatches = ' + str(correctPatches))
+
 def checkCorrectFix(projPath: Path, patchDirPath: Path):
-    fixedMidList = []
+    fixedMidList = set()
+    correctPatches = []
     projSrcRelativePath = sp.check_output("defects4j export -p dir.src.classes", shell=True, universal_newlines=True, cwd=str(projPath), stderr=sp.DEVNULL).strip()
     for patchFile in patchDirPath.iterdir():
         if str(patchFile).endswith('.txt'):
@@ -120,12 +133,13 @@ def checkCorrectFix(projPath: Path, patchDirPath: Path):
             pid = patchFile.stem.split('-')[0]
             fixedLine = getMutFixedLine(projPath, mid, projSrcPath=projSrcRelativePath)
             with patchFile.open() as f:
+                patchIdx = 1
                 for line in f:
                     if isExactlySameCode(line, fixedLine):
-                        fixedMidList.append(mid)
-                        break
-    # print("{} mutants of {} are correctly (exactly) fixed!".format(len(fixedMidList), pid))
-    return fixedMidList
+                        fixedMidList.add(mid)
+                        correctPatches.append('{}-{}-{}'.format(pid, mid, patchIdx))
+                    patchIdx += 1
+    return list(fixedMidList), correctPatches
 
 def isExactlySameCode(a:str, b:str):
     tmp1 = ''.join(a.split())
@@ -251,7 +265,7 @@ def collectSourcePatches():
 #     print(len(getSampledMutIdList(path)))
 
 if __name__ == '__main__':
-    os.environ['CUDA_VISIBLE_DEVICES'] = "2"
+    os.environ['CUDA_VISIBLE_DEVICES'] = "0"
     os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
     # main()
     # recoverAndCheckCorrectFixes()
